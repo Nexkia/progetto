ab3c44868f574d441bbe27538a1e7aeb3f9d24a4
Giuseppe <giuseppe794@yahoo.it>
2017-10-26 19:03:29 +0200

    sync

---
+++ ServiceAPI/ServiceAPI/Dal/Student.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceAPI.Dal
+{
+
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string BirthPlace { get; set; }
+
+
+        public string RegistrationNumber { get; set; }
+
+        public bool Gender { get; set; } //true male,false female
+
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ ServiceAPI/ServiceAPI/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using ServiceAPI.Dal;
+using System;
+using System.Threading.Tasks;
+
+namespace ServiceAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var context = new StudentsDbContext())
+            {
+                // Create database
+                context.Database.EnsureCreated();
+
+                Student s = new Student()
+                {
+                    Name = "Giovanni",
+                    DateOfBirth = new DateTime(2012, 1, 1),
+                };
+
+                context.Students.Add(s);
+
+                context.SaveChanges();
+            }
+
+
+            //var host = new WebHostBuilder()
+            //    .UseKestrel()
+            //    .UseStartup<Startup>()
+            //    .Build();
+
+            //Task restService = host.RunAsync();
+
+            ////System.Diagnostics.Process.Start("chrome.exe", "http://localhost/netcoreapp2.0/corsoing/");
+            //System.Diagnostics.Process.Start("cmd", "/C start http://localhost/netcoreapp2.0/corsoing/");
+            //restService.Wait();
+        }
+    }
+}
